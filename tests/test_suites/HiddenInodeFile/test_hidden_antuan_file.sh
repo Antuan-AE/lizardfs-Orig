7c770f97c55bf1ea24ef8f6a937bfcf45e640ea8
Antuan <antuan@aleph.engineering>
2021-07-19 01:15:14 -0400

    test: Added Test for Special Inode
    Added test for Special Inode for "Hello World" example.

---
+++ tests/test_suites/HiddenInodeFile/test_hidden_antuan_file.sh
@@ -0,0 +1,10 @@
+CHUNKSERVERS=1 \
+	USE_RAMDISK=YES \
+	setup_local_empty_lizardfs info
+
+cd ${info[mount0]}
+
+EXPECTED_TEXT="Hello world!"
+TEXT="$(cat .antuan)"
+
+expect_equals "$TEXT" "$EXPECTED_TEXT"
